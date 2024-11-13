0b6fb3e0d42c9bd7efb5e5e8569a1613d07beb1c
PaulAlbon <paul_albon@outlook.com>
2024-11-13 15:22:34 +0200

    fixed merge issue

--- src/CHIPBindings.cc
+++ src/CHIPBindings.cc
@@ -1010,11 +1010,22 @@ hipError_t hipGraphAddDependencies(hipGraph_t graph, const hipGraphNode_t *from,
   if (!graph || !from || !to)
     RETURN(hipErrorInvalidValue);
   CHIPInitialize();
-  chipstar::GraphNode *FoundNode = GRAPH(graph)->findNode(NODE(*to));
-  if (!FoundNode)
-    RETURN(hipErrorInvalidValue);
 
-  FoundNode->addDependencies(DECONST_NODES(from), numDependencies);
+  NULLCHECK(graph);
+  if (numDependencies)
+    NULLCHECK(from, to);
+  chipstar::Graph *G = GRAPH(graph);
+  for (size_t i = 0; i < numDependencies; i++) {
+    chipstar::GraphNode *FoundNode = G->findNode(NODE(to[i]));
+    if (!FoundNode)
+      RETURN(hipErrorInvalidValue);
+    FoundNode = G->findNode(NODE(from[i]));
+    if (!FoundNode)
+      RETURN(hipErrorInvalidValue);
+  }
+  for (size_t i = 0; i < numDependencies; i++) {
+    G->findNode(NODE(to[i]))->addDependency(G->findNode(NODE(from[i])));
+  }
   RETURN(hipSuccess);
   CHIP_CATCH
 }
